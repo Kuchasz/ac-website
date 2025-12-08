7f065e4ea4030675066310c8ef0aeff4f1ff70d4
Kuchasz <kuchasz1@o2.pl>
2025-12-09 00:34:37 +0100

    codegen comment out

--- codegen.ts
+++ codegen.ts
@@ -1,17 +1,17 @@
-import type { CodegenConfig } from '@graphql-codegen/cli';
-import { loadEnvConfig } from "@next/env";
+// import type { CodegenConfig } from '@graphql-codegen/cli';
+// import { loadEnvConfig } from "@next/env";
 
-loadEnvConfig(process.cwd());
+// loadEnvConfig(process.cwd());
 
-const config: CodegenConfig = {
-  overwrite: true,
-  schema: process.env.API_URL,
-  documents: ["src/queries/*.graphql"],
-  generates: {
-    "src/gql.ts": {
-      plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request']
-    }
-  }
-};
+// const config: CodegenConfig = {
+//   overwrite: true,
+//   schema: process.env.API_URL,
+//   documents: ["src/queries/*.graphql"],
+//   generates: {
+//     "src/gql.ts": {
+//       plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request']
+//     }
+//   }
+// };
 
-export default config;
+// export default config;
